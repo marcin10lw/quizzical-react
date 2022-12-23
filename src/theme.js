b45d957c347526cbfe17242a3a9b4641cf8d26ed
marcin10lw <marcin10lw@wp.pl>
2022-12-23 20:44:30 +0100

    initial commit

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    whiteLilac: "#F5F7FB",
+    rhino: "#293264",
+    kashmirBlue: "#4D5B9E",
+    linkWater: "#D6DBF5",
+    gray: "#DBDEF0",
+    vistaBlue: "#94D7A2",
+    pink: "#F8BCBC",
+  },
+};
