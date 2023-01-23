4ecc4e86fbdc0dbc75774bfa9db312f5e721fdb0
marcin10lw <marcin10lw@wp.pl>
2023-01-23 19:50:34 +0100

    created questionsSaga

---
+++ src/features/questions/questionsSaga.js
@@ -0,0 +1,18 @@
+import { call, put, takeEvery } from "redux-saga/effects";
+import { getQuestions } from "./getQuestions";
+import { fetchQuestions, setQuestions, setStatus } from "./questionsSlice";
+
+function* fetchQuestionsHandler() {
+  try {
+    yield put(setStatus("pending"));
+    const questions = yield call(getQuestions);
+    yield put(setQuestions(questions));
+    yield put(setStatus("success"));
+  } catch (error) {
+    yield put(setStatus("error"));
+  }
+}
+
+export function* watchFetchQuestions() {
+  yield takeEvery(fetchQuestions.type, fetchQuestionsHandler);
+}
--- src/store.js
+++ src/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import questionsReducer from "./features/questions/questionsSlice";
+import createSagaMiddleware from "@redux-saga/core";
+import { watchFetchQuestions } from "./features/questions/questionsSaga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
     questions: questionsReducer,
   },
+  middleware: [sagaMiddleware],
 });
+
+sagaMiddleware.run(watchFetchQuestions);
